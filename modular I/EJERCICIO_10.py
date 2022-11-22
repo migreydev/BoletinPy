788e5f1d34ccfddbfb1442eb0f43aa8ba64d4192
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2022-11-22 20:45:39 +0100

    Delete modular I directory

--- modular I/EJERCICIO_10.py
+++
@@ -1,5 +0,0 @@
-"""10. Diseña una función conversor que convierta un número de binario a decimal o de
-decimal a binario. Esta función recibirá un número en formato de cadena de texto
-cuya última posición indica el sistema numérico utilizado (D-decimal, B-binario).
-Debe validar la información, así, por ejemplo, el número ‘1020101B’ no sería válido
-puesto que los valores en binario son 0 y 1."""
